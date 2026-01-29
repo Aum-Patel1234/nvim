d152288c4c96140b06774677c21df3e2d623836b
Aum Patel <aumpatel131@gmail.com>
2026-01-30 00:04:42 +0530

    disable noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/noice.nvim",
+  enabled = false,
+}
+
+-- return {
+--   "folke/noice.nvim",
+--   opts = {
+--     cmdline = {
+--       enabled = false, -- disables popup cmdline
+--     },
+--     messages = {
+--       enabled = true,
+--     },
+--     popupmenu = {
+--       enabled = true,
+--     },
+--   },
+-- }
